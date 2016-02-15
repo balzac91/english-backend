1b93c6cb54049c341956ab03bd97d7e38138e105
Kamil Fus <balzac91@gmail.com>
2016-02-15 16:18:19 +0100

    add Tools controller

---
+++ src/Controller/ToolsController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controller;
+
+use Cake\Core\Configure;
+
+class ToolsController extends AppController
+{
+    public function index()
+    {
+        $words = array();
+
+        $page = 'http://www.ang.pl/slownictwo/slownictwo-angielskie-poziom-a1';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $page);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        $result = curl_exec($ch);
+
+
+        preg_match_all('/<li><a href="\/slownictwo\/(.*)">([0-9]+)<\/a><\/li>/', $result, $pagination);
+        $pages = end($pagination[2]);
+
+        for ($i = 1; $i <= $pages; $i++) {
+            $url = $page . '/page/' . $i;
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+            $result = curl_exec($ch);
+
+            preg_match_all('/<div class="large-4 columns"><p class="big (a1|a2|b1|b2|c1|c2)">\s*<a href="(.*)" class="sm2_button">(.*)<\/a>\s*(.*)<\/p><\/div>\s*<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>\s*<\/div>/', $result, $wordsResult);
+
+            foreach ($wordsResult[4] as $key => $word) {
+                if ($word && $wordsResult[5][$key]) {
+                    $words[] = $word . ' - ' . $wordsResult[5][$key];
+                }
+            }
+
+
+        }
+
+        pr($words);
+        die();
+
+        curl_close($ch);
+        die();
+
+//preg_match_all('/<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>/', $titlePage, $title);
+//preg_match_all('/class="sm2_button">(.*)<\/a>/', $titlePage, $title);
+//preg_match_all('/<div class="large-4 columns"><p class="big (.*)"><a href="(.*)" class="(.*)">(.*)<\/a>(.*)<\/p><\/div><div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div><\/div>/', $titlePage, $title);
+        preg_match_all('/<div class="large-4 columns"><p class="big (a1|a2|b1|b2|c1|c2)">\s*<a href="(.*)" class="sm2_button">(.*)<\/a>\s*(.*)<\/p><\/div>\s*<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>\s*<\/div>/', $titlePage, $title);
+
+
+        pr($title);
+        die();
+    }
+}
