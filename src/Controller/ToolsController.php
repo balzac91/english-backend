6d68852b1a14f5f0bea8a5c6633136f7e8bba165
Kamil Fus <balzac91@gmail.com>
2016-02-16 18:08:11 +0100

    getWords method in tools

--- src/Controller/ToolsController.php
+++ src/Controller/ToolsController.php
@@ -7,62 +7,13 @@ use Cake\ORM\TableRegistry;
 
 class ToolsController extends AppController
 {
-    public function index()
+    public function getCategories()
     {
-        $words = array();
-
-        $page = 'http://www.ang.pl/slownictwo/slownictwo-angielskie-poziom-a1';
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $page);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-        $result = curl_exec($ch);
-
-
-        preg_match_all('/<li><a href="\/slownictwo\/(.*)">([0-9]+)<\/a><\/li>/', $result, $pagination);
-        $pages = end($pagination[2]);
-
-        for ($i = 1; $i <= $pages; $i++) {
-            $url = $page . '/page/' . $i;
-
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-            $result = curl_exec($ch);
-
-            preg_match_all('/<div class="large-4 columns"><p class="big (a1|a2|b1|b2|c1|c2)">\s*<a href="(.*)" class="sm2_button">(.*)<\/a>\s*(.*)<\/p><\/div>\s*<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>\s*<\/div>/', $result, $wordsResult);
-
-            foreach ($wordsResult[4] as $key => $word) {
-                if ($word && $wordsResult[5][$key]) {
-                    $words[] = $word . ' - ' . $wordsResult[5][$key];
-                }
-            }
-
-
-        }
-
-        pr($words);
-        die();
-
-        curl_close($ch);
-        die();
-
-//preg_match_all('/<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>/', $titlePage, $title);
-//preg_match_all('/class="sm2_button">(.*)<\/a>/', $titlePage, $title);
-//preg_match_all('/<div class="large-4 columns"><p class="big (.*)"><a href="(.*)" class="(.*)">(.*)<\/a>(.*)<\/p><\/div><div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div><\/div>/', $titlePage, $title);
-        preg_match_all('/<div class="large-4 columns"><p class="big (a1|a2|b1|b2|c1|c2)">\s*<a href="(.*)" class="sm2_button">(.*)<\/a>\s*(.*)<\/p><\/div>\s*<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>\s*<\/div>/', $titlePage, $title);
-
-
-        pr($title);
-        die();
-    }
-
-    public function getCategories() {
+        set_time_limit(0);
         $this->autoRender = false;
 
         $http = new Client();
-        $response = $http->get('http://www.ang.pl/slownictwo/tematyczne');
+        $response = $http->get('http://www.ang.pl/slownictwo/tematyczne/');
         preg_match_all('/<h2><a href="([a-z0-9\/-]+)">([a-żA-Ż0-9\(\),\- ]*)<\/a><\/h2>/', $response->body(), $result);
 
         $categoriesTable = TableRegistry::get('Categories');
@@ -71,6 +22,69 @@ class ToolsController extends AppController
             $category->name = $result[2][$key];
             $category->url = $result[1][$key];
             $categoriesTable->save($category);
+        }
+    }
+
+    public function getWords()
+    {
+        set_time_limit(0);
+        $this->autoRender = false;
+
+        $http = new Client();
+        $mainUrl = 'http://www.ang.pl';
+
+
+        $levelsTable = TableRegistry::get('Levels');
+        $levels = $levelsTable->find('all');
+
+        $levelsData = array();
+        foreach ($levels as $level) {
+            $levelsData[$level->name] = $level->id;
+        }
+
+        $categoriesTable = TableRegistry::get('Categories');
+        $categories = $categoriesTable->find('all');
+
+        $wordsTable = TableRegistry::get('Words');
+
+        foreach ($categories as $category) {
+            $response = $http->get($mainUrl . $category->url);
+            preg_match_all('/<li><a href="\/slownictwo\/(.*)">([0-9]+)<\/a><\/li>/', $response->body(), $pagination);
+
+            $pages = 1;
+            if (!empty($pagination[2])) {
+                $pages = end($pagination[2]);
+            }
+
+            for ($i = 1; $i <= $pages; $i++) {
+                $url = $mainUrl . $category->url . '/page/' . $i;
+                $response = $http->get($url);
+                preg_match_all('/<div class="large-4 columns"><p class="big (a1|a2|b1|b2|c1|c2)?">\s*<a href="(.*)" class="sm2_button">(.*)<\/a>\s*(.*)<\/p><\/div>\s*<div class="large-4 columns end"><p><em>(.*)<\/em><\/p><\/div>\s*<\/div>/', $response->body(), $wordsData);
+
+                foreach ($wordsData[4] as $key => $item) {
+                    if ($item && $wordsData[5][$key]) {
+                        $levelId = ($wordsData[1][$key]) ? $levelsData[$wordsData[1][$key]]: null;
+                        $polish = $wordsData[5][$key];
+
+                        $count = $wordsTable->find('all')
+                            ->where(array(
+                                'category_id' => $category->id,
+                                'polish' => $polish,
+                                'english' => $item
+                            ))
+                            ->count();
+
+                        if (!$count) {
+                            $word = $wordsTable->newEntity();
+                            $word->category_id = $category->id;
+                            $word->level_id = $levelId;
+                            $word->polish = $polish;
+                            $word->english = $item;
+                            $wordsTable->save($word);
+                        }
+                    }
+                }
+            }
         }
     }
 }
